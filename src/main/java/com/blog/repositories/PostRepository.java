62e5f50dbf95a9766be8eb032afa96b369e471c8
jitu0909 <142646887+jitu0909@users.noreply.github.com>
2024-01-16 11:21:49 +0530

    Delete src/main/java/com/blog/repositories directory

--- src/main/java/com/blog/repositories/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package com.blog.repositories;
-
-import com.blog.entities.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-
-}
