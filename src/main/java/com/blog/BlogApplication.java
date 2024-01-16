2cddf3c017c06e19c2c11e5e3ef06ae316ad53a7
jitu0909 <142646887+jitu0909@users.noreply.github.com>
2024-01-16 11:23:22 +0530

    Update BlogApplication.java

--- src/main/java/com/blog/BlogApplication.java
+++ src/main/java/com/blog/BlogApplication.java
@@ -6,11 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class BlogApplication {
+public class GiftyApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(BlogApplication.class, args);
+		SpringApplication.run(GiftyApplication.class, args);
 
 	}
 
